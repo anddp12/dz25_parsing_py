e321bfcc49cbdde15712dc274b684956da16f032
anddp12 <anddp12@gmail.com>
2023-01-26 01:37:27 +0200

    parsing data of the table header

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
+from datetime import datetime
+import csv
 
 # https://football.ua/italy.html
-
+cur_date = datetime.now().strftime('%Y_%m_%d')
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0'}
 # ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
-resp = requests.get('https://football.ua/italy.html', headers=headers).text
+resp = requests.get('https://football.ua/italy/table.html', headers=headers).text
 # resp.encoding = 'utf'
 soup = BeautifulSoup(resp, 'html.parser')
 # print(resp)
@@ -18,7 +20,22 @@ with open('index.html', encoding='utf8') as file:
     src = file.read()
 
 soup1 = BeautifulSoup(src, 'lxml')
-table = soup1.find('table', class_='tournament-table')
-table_tr = table.find('tr')
-# print(table)
-print(table_tr)
+table = soup1.find('table', class_='main-tournament-table')
+date_th = table.find('tr').find_all('th')
+# print(date_th)
+
+table_th = []
+for tr in date_th:
+    tr = tr.text.strip()
+    table_th.append(tr)
+# print(table_th)
+
+with open(f'date{cur_date}.csv','w',encoding='utf8')as file:
+    writen = csv.writer(file)
+    writen.writerow(
+        (
+            table_th
+        )
+    )
+
+
